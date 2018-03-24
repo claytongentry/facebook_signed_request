bd09905b844ba83294451767e413507021af57b3
claytongentry <claytongentry2017@u.northwestern.edu>
2018-03-24 11:31:41 -0400

    updates

--- lib/facebook_signed_request.ex
+++ lib/facebook_signed_request.ex
@@ -1,13 +1,13 @@
 defmodule FacebookSignedRequest do
   @moduledoc """
-  This module provides methods for checking the validity of a signed request 
-  and extracting the data out of it. 
+  This module provides methods for checking the validity of a signed request
+  and extracting the data out of it.
   """
 
   @doc """
   Checks if the signed request is valid or not.
   """
-  def valid?(signed_request, app_secret \\ get_app_secret()) do
+  def valid?(signed_request, app_secret) do
     {signature, payload} = split_signed_request(signed_request)
     check_signature(signature, payload, app_secret)
   end
@@ -15,31 +15,25 @@ defmodule FacebookSignedRequest do
   @doc """
   Extracts the data of the signed request.
   """
-  def data(signed_request, app_secret \\ get_app_secret()) do
+  def data(signed_request, app_secret) do
     {signature, payload} = split_signed_request(signed_request)
+    valid? = check_signature(signature, payload, app_secret)
 
-    case check_signature(signature, payload, app_secret) do
-      true -> payload |> Base.decode64!(padding: false) |> Poison.decode!
-      false -> {:error, "Signed Request is invalid!"}
-      other -> other
+    if valid? do
+
+      case Base.decode64(payload, padding: false) do
+        {:ok, payload} -> Poison.decode(payload)
+        _error         -> {:error, :base_64_decode_error}
+      end
+
+    else
+      {:error, :invalid_request}
     end
   end
 
-  defp check_signature(signature, payload, app_secret) do
-    case app_secret do
-      nil -> {:error, "You need to pass or set an app secret!"}
-      _ -> check_signature!(signature, payload, app_secret)
-    end
-  end
-
-  defp check_signature!(signature, payload, app_secret) do
-    signature = signature |> prepare_base64_url
-    case sha256_hmac(payload, app_secret) do
-      ^signature ->
-        true
-      _ ->
-        false
-    end
+  defp check_signature(_signature, _payload, nil), do: false
+  defp check_signature(signature, payload, secret) do
+    sha256_hmac(payload, secret) == prepare_base64_url(signature)
   end
 
   defp split_signed_request(signed_request) do
@@ -50,17 +44,15 @@ defmodule FacebookSignedRequest do
 
   defp sha256_hmac(payload, app_secret) do
     :crypto.hmac(:sha256, app_secret, payload)
-    |> Base.encode64
-    |> String.strip
+    |> Base.encode64()
+    |> String.trim()
   end
 
   defp prepare_base64_url(str) do
     mode = str |> String.length |> rem(4)
     equal_signs = "=" |> String.duplicate(4 - mode)
-    str <> equal_signs 
+    str <> equal_signs
     |> String.replace("-","+")
     |> String.replace("_","/")
   end
-
-  defp get_app_secret, do: System.get_env("FACEBOOK_SIGNED_REQUEST_APP_SECRET")
 end
